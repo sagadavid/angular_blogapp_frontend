c51dcbb89f238b7ffc8b5a6e7fecc2cc0d16acab
sagadavid <epostsaga@gmail.com>
2023-07-06 15:45:29 +0200

    user model meets login service

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,9 @@
+export class User {
+  private username = '';
+  private password = '';
+
+  constructor(username: string, password: string) {
+    this.username = username;
+    this.password = password;
+  }
+}
